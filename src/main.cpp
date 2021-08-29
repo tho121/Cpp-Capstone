bb261ae2159dc17b68b226db4c0602c7f68bc144
Tony Ho <tho121@hotmail.com>
2021-08-29 15:02:39 -0700

    Sets up opencv

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
 
 int main() {
     std::cout << "Hello World!" << "\n";
+
+    Mat image = imread("./PetImages/Cat/0.jpg");
+
     return 0;
 }
