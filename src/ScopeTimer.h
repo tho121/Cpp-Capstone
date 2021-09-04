c6ff6020e089a53e7c3b5e32417ec3b8f5d08622
Tony Ho <tho121@hotmail.com>
2021-09-03 18:54:40 -0700

    Refactors for udacity rubric (ro5, condition variable)

--- src/ScopeTimer.h
+++ src/ScopeTimer.h
@@ -4,23 +4,95 @@
 
 using namespace std;
 
+template<typename T>
 class ScopeTimer
 {
 public:
-    ScopeTimer(string msg)
+    
+    ScopeTimer(const char* msg)
+    :   startPoint_(chrono::high_resolution_clock::now())
     {
-        msg_ = msg;
-        startPoint_ = chrono::high_resolution_clock::now();
+        msg_ = new char[256];
+        memset(msg_, '\0', 256);
+        strcpy(msg_, msg);
     }
 
     ~ScopeTimer()
     {
-        auto duration = chrono::duration_cast<chrono::minutes>(chrono::high_resolution_clock::now() - startPoint_).count();
+        printMsg();
 
-        cout << msg_ << " " << duration << endl;
+        delete [] msg_;
     }
 
-private:
-    string msg_;
+    ScopeTimer(const ScopeTimer& other)
+    {
+        strcpy(msg_, other.msg_);
+        startPoint_ = other.startPoint_;
+    }
+
+    ScopeTimer& operator=(const ScopeTimer& other)
+    {
+        strcpy(msg_, other.msg_);
+        startPoint_ = other.startPoint_;
+
+        return *this;
+    }
+
+    ScopeTimer(ScopeTimer&& other)
+    {
+        msg_ = other.msg_;
+        other.msg_ = nullptr;
+
+        startPoint_ = std::move(other.startPoint_);
+    }
+
+    ScopeTimer& operator=(ScopeTimer&& other)
+    {
+        msg_ = other.msg_;
+        other.msg_ = nullptr;
+
+        startPoint_ = std::move(other.startPoint_);
+
+        return *this;
+    }
+
+    int getCurrentDuration()
+    {
+        return chrono::duration_cast<T>(chrono::high_resolution_clock::now() - startPoint_).count();
+    }
+
+    virtual void printMsg()
+    {
+        cout << "Timer - " << msg_ << " " << getCurrentDuration() << endl;
+    }
+
+protected:
+    char* msg_;
     chrono::time_point<std::chrono::high_resolution_clock> startPoint_;
+};
+
+class ScopeTimerSec : public ScopeTimer<chrono::seconds>
+{
+public:
+    ScopeTimerSec(const char* msg)
+    :   ScopeTimer<chrono::seconds>(msg)
+    {}
+
+    void printMsg()
+    {
+        cout << "Timer - " << msg_ << " " << getCurrentDuration() << "s" << endl;
+    }
+};
+
+class ScopeTimerMin : public ScopeTimer<chrono::minutes>
+{
+public:
+    ScopeTimerMin(const char* msg)
+    :   ScopeTimer<chrono::minutes>(msg)
+    {}
+
+    void printMsg()
+    {
+        cout << "Timer - " << msg_ << " " << getCurrentDuration() << "m" << endl;
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,9 @@
 #include "ScopeTimer.h"
 
 #include <iostream>
+#include <condition_variable>
 #include <string>
-#include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -13,7 +14,7 @@ int main()
 {
     std::cout << "Hello World!" << "\n";
 
-    const int sampleSize = 5000;
+    const int sampleSize = 200;
     const Size imageSize(300, 300);
     const int dictionarySize = 100;
     const int threadCount = 4;
@@ -25,26 +26,28 @@ int main()
 
     ImageContainer train_ic(paths, sampleSize, imageSize);
 
-    BagTrainer bt(dictionarySize, threadCount);
+    BagTrainer bagTrainer(dictionarySize, threadCount);
+
     {
-        ScopeTimer t("Computed descriptors: ");
-        bt.computeDescriptors(train_ic.getImages());
+        ScopeTimer<chrono::milliseconds> t("Computed descriptors: ");
+
+        bagTrainer.computeDescriptors(train_ic.getImages());
     }
     
     {
-        ScopeTimer t("Computed vocab: ");
-        bt.setVocab(maxBagIterations);
+        ScopeTimer<chrono::milliseconds> t("Computed vocab: ");
+        bagTrainer.setVocab(maxBagIterations);
     }
 
     Mat labels;
     Mat trainData;
     {
-        ScopeTimer t("Prepared training data: ");
+        ScopeTimerMin t("Prepared training data: ");
         vector<Mat> catImages = train_ic.getImages(0);
-        Mat catData = bt.getDescriptors(catImages);
+        Mat catData = bagTrainer.getDescriptors(catImages);
 
         vector<Mat> dogImages = train_ic.getImages(1);
-        Mat dogData = bt.getDescriptors(dogImages);
+        Mat dogData = bagTrainer.getDescriptors(dogImages);
 
         for(int i = 0; i < catData.rows; ++i) {labels.push_back(1);}
         for(int i = 0; i < dogData.rows; ++i) {labels.push_back(-1);}
@@ -52,26 +55,75 @@ int main()
         vconcat(catData, dogData, trainData);
     }
     
-    SVMTrainer st(maxSVMIterations);
+    SVMTrainer svmTrainer(maxSVMIterations);
+
     {
-        ScopeTimer t("Training SVM Done: ");
-        st.train(trainData, labels);
+        ScopeTimerMin t("Training SVM Done: ");
+        svmTrainer.train(trainData, labels);
     }
     
     ImageContainer test_ic(paths, testSize, imageSize, sampleSize);
 
-    Mat cat_testData = bt.getDescriptors(test_ic.getImages(0));
-    Mat dog_testData = bt.getDescriptors(test_ic.getImages(1));
+    //////////////////////
+
+    /* Mat cat_testData = bagTrainer.getDescriptors(test_ic.getImages(0));
+    Mat dog_testData = bagTrainer.getDescriptors(test_ic.getImages(1));
 
     cout << "Testing Cat labels" << endl;
 
     Mat catLabels(cat_testData.rows, 1, CV_32F, 1);
-    st.test(cat_testData, catLabels);
+    svmTrainer.test(cat_testData, catLabels);
 
     cout << "Testing Dog labels" << endl;
 
     Mat dogLabels(dog_testData.rows, 1, CV_32F, -1);
-    st.test(dog_testData, dogLabels);
+    svmTrainer.test(dog_testData, dogLabels); */
+
+    //////////////////////
+
+    std::condition_variable condition;
+    std::mutex mutex;
+
+    Mat testMat;
+    int testIndex;
+    
+
+    auto testData = [&testMat, &svmTrainer, &testIndex, &condition, &mutex]() mutable {
+        
+        int testCount = 0;
+
+        do{
+            std::unique_lock<std::mutex> lck(mutex);
+            condition.wait(lck, [&testMat]{ return testMat.rows > 0; });
+
+            testIndex == 0 ? cout << "Testing Cat labels" << endl
+                : cout << "Testing Dog labels" << endl;
+
+            Mat labels(testMat.rows, 1, CV_32F, testIndex == 0 ? 1 : -1 );
+            svmTrainer.test(testMat, labels);
+            testMat = Mat();    //reset
+            testCount++;
+            
+        } while (testCount < 2);
+    };
+
+    auto getTestData = [&testMat, &bagTrainer, &test_ic, &testIndex, &condition, &mutex] (int index) mutable {
+        Mat testData = bagTrainer.getDescriptors(test_ic.getImages(index));
+
+        std::unique_lock<std::mutex> lck(mutex);
+        testMat = testData;
+        testIndex = index;
+
+        condition.notify_one();
+    };
+
+    std::thread testThread(testData);
+    std::thread catThread(getTestData, 0);
+    std::thread dogThread(getTestData, 1);
+
+    testThread.join();
+    catThread.join();
+    dogThread.join();
 
     return 0;
 }
